918d87810498b329b84b00ac3b1cb055e670b319
MagedAhmed <maged.abdelmoniem@gmail.com>
2024-12-04 23:18:45 +0000

    Completed Navbar setup and stylying

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,36 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { FaCompass, FaTag, FaUser } from "react-icons/fa";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function isActive(route) {
+    return location.pathname === route ? "#2c2c2c" : "#8f8f8f";
+  }
+
   return (
     <footer className="navbar">
       <nav className="navbarNav">
         <ul className="navbarListItems">
-          <li className="navbarListItem">
-            <FaCompass style={{ width: "36px", height: "36px" }} />
+          <li className="navbarListItem" onClick={() => navigate("/")}>
+            <FaCompass
+              color={isActive("/")}
+              style={{ width: "30px", height: "30px" }}
+            />
             <p>Explore</p>
           </li>
-          <li className="navbarListItem">
-            <FaTag style={{ width: "36px", height: "36px" }} />
+          <li className="navbarListItem" onClick={() => navigate("/offers")}>
+            <FaTag
+              color={isActive("/offers")}
+              style={{ width: "30px", height: "30px" }}
+            />
             <p>Offers</p>
           </li>
-          <li className="navbarListItem">
-            <FaUser style={{ width: "36px", height: "36px" }} />
+          <li className="navbarListItem" onClick={() => navigate("/profile")}>
+            <FaUser
+              color={isActive("/profile")}
+              style={{ width: "30px", height: "30px" }}
+            />
             <p>Profile</p>
           </li>
         </ul>
